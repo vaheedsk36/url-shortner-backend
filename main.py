f36964071e90929b6a98c874b0f2a91fce8ccfba
Abhiram Singuru <abhiramsinguru@Abhirams-MacBook-Air.local>
2024-08-31 16:31:42 +0530

    Added typr to URLRequest function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+from pydantic import BaseModel  # Import Pydantic's BaseModel
 import string
 import random
 
@@ -28,13 +29,17 @@ def get_db():
 
 def generate_short_url():
     characters = string.ascii_letters + string.digits
-    short_url = ''.join(random.choices(characters, k=6))
+    short_url = ''.join(random.choices(characters, k=3))
     return short_url
 
+# Define a Pydantic model for the request body
+class URLRequest(BaseModel):
+    original_url: str
+
 @app.post("/shorten/")
-def shorten_url(original_url: str, db: Session = Depends(get_db)):
+def shorten_url(request: URLRequest, db: Session = Depends(get_db)):
     short_url = generate_short_url()
-    db_url = URL(short_url=short_url, original_url=original_url)
+    db_url = URL(short_url=short_url, original_url=request.original_url)
     db.add(db_url)
     db.commit()
     db.refresh(db_url)

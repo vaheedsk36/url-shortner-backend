49000695ed66e49a07637bad903274c78486c1e7
vaheed <vaheedsk36@gmail.com>
2024-08-31 18:58:37 +0530

    chore(app): [1] added run_server.py [2] added Dockerfile

--- main.py
+++ main.py
@@ -28,6 +28,15 @@ def get_db():
         db.close()
 
 def generate_short_url():
+    """
+    Generates a short URL of a specified length.
+    
+    Parameters:
+    None
+    
+    Returns:
+    str: A short URL of length 3, consisting of random alphanumeric characters.
+    """
     characters = string.ascii_letters + string.digits
     short_url = ''.join(random.choices(characters, k=3))
     return short_url
@@ -42,6 +51,16 @@ def health_test():
 
 @app.post("/shorten/")
 def shorten_url(request: URLRequest, db: Session = Depends(get_db)):
+    """
+    Shortens a given URL by generating a unique short URL and storing it in the database.
+
+    Args:
+        request (URLRequest): The request containing the original URL.
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        dict: A dictionary containing the generated short URL.
+    """
     short_url = generate_short_url()
     db_url = URL(short_url=short_url, original_url=request.original_url)
     db.add(db_url)
@@ -51,6 +70,19 @@ def shorten_url(request: URLRequest, db: Session = Depends(get_db)):
 
 @app.get("/{short_url}")
 def redirect_to_url(short_url: str, db: Session = Depends(get_db)):
+    """
+    Redirects to the original URL based on the provided short URL.
+
+    Args:
+    short_url (str): The short URL to redirect from.
+    db (Session): The database session. Defaults to Depends(get_db).
+
+    Returns:
+    dict: A dictionary containing the original URL.
+
+    Raises:
+    HTTPException: If the short URL is not found in the database.
+    """
     db_url = db.query(URL).filter(URL.short_url == short_url).first()
     if db_url is None:
         raise HTTPException(status_code=404, detail="URL not found")

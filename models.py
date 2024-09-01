a0da44706c89a85472826a11c46133abda52c057
Abhiram Singuru <singuruabhiram5123@gmail.com>
2024-09-01 12:22:07 +0530

    removed comments from models.py

--- models.py
+++ models.py
@@ -1,20 +1,3 @@
-# from sqlalchemy import Column, String, Integer, create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = "sqlite:///./test.db"
-
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
-
-# class URL(Base):
-#     __tablename__ = "urls"
-#     id = Column(Integer, primary_key=True, index=True)
-#     short_url = Column(String, unique=True, index=True)
-#     original_url = Column(String)
-
-# Base.metadata.create_all(bind=engine)
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 import os
